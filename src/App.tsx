b19ca8ed884ff514c1ab36ee581a911a943708dd
Bartosz Wojtczak <bartosz.wojtczak@tellyo.com>
2023-06-25 16:05:31 +0200

    midi connection

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import * as Tone from "tone";
 import * as fs from "fs";
 import * as path from "path";
 import { parseArrayBuffer } from "midi-json-parser";
+import { ipcMain, ipcRenderer } from "electron";
 
 // ...
 
@@ -15,28 +16,107 @@ const App: React.FC = () => {
 		// 	window.electron.appDir,
 		// 	midiFilePath
 		// );
-		console.log(window.electron.fs.readdirSync("./"));
+		// console.log(window.electron.fs.readdirSync("./"));
+		// const midiData = window.electron.fs.readFileSync("./src/assets/mozart.mid");
+		// const midi = new Midi(midiData);
+		// console.log(midi);
+		// console.log(midi.tracks[0].notes);
+		// console.log(Tone);
+		// const synth = new Tone.Synth().toDestination();
+		// Tone.start();
+		// midi.tracks[0].notes.forEach((note) => {
+		// 	synth.triggerAttackRelease(
+		// 		note.name,
+		// 		note.duration,
+		// 		note.time,
+		// 		note.velocity
+		// 	);
+		// });
+		// const midiToJson = window.electronAPI.generateMidi();
+		// console.log(
+		// 	window.electron,
+		// 	window.electronAPI,
+		// 	midiToJson,
+		// 	window.electron.ipcMain
+		// );
+		// // window.electron.ipcMain.on("dialog:generateMidi", (event, args) => {
+		// // 	// Your code to handle the 'dialog:generateMidi' event goes here
+		// // 	// After processing, you can reply back to the renderer process like so:
+		// // 	console.log(event);
+		// // 	event.reply("dialog:generateMidi-response", "Your response goes here");
+		// // });
+		// window.electron.ipcRenderer.on("dialog:generateMidi", (event, args) => {
+		// 	console.log(event, args);
+		// });
 
-		const midiData = window.electron.fs.readFileSync("./src/assets/mozart.mid");
-
-		const midi = new Midi(midiData);
-		console.log(midi);
-		console.log(midi.tracks[0].notes);
-		console.log(Tone);
-		const synth = new Tone.Synth().toDestination();
-		Tone.start();
-
-		midi.tracks[0].notes.forEach((note) => {
-			synth.triggerAttackRelease(
-				note.name,
-				note.duration,
-				note.time,
-				note.velocity
-			);
+		window.electronAPI.receive("app_ready", () => {
+			// Put the code to invoke 'dialog:openFile' here
+			console.log("aha");
 		});
+
+		const updateDevices = (event) => {
+			console.log(
+				`Name ${event.port.name}, brand: ${event.port.manufacturer}, State, ${event.port.state}, Type: ${event.port.type}`
+			);
+		};
+
+		const noteOn = (note, velocity) => {
+			console.log(note, velocity);
+		};
+
+		const noteOff = (note) => {
+			console.log(note);
+		};
+
+		const handleInput = (input) => {
+			const [command, note, velocity] = input.data;
+			switch (command) {
+				case 144: // noteOn
+					if (velocity > 0) {
+						noteOn(note, velocity);
+					} else {
+						noteOff(note);
+					}
+					break;
+				case 128: // note off
+					noteOff(note);
+					break;
+			}
+		};
+
+		const success = (midiAccess) => {
+			console.log(midiAccess);
+			// midiAccess.onstatechange = updateDevices;
+			midiAccess.addEventListener("statechange", updateDevices);
+
+			const inputs = midiAccess.inputs;
+			console.log(inputs);
+			inputs.forEach((input) => {
+				// console.log(input);
+				input.onmidimessage = handleInput;
+				input.addEventListener("midimessage", handleInput);
+			});
+		};
+		const failure = () => {
+			console.log("Failure");
+		};
+
+		if (navigator.requestMIDIAccess) {
+			navigator.requestMIDIAccess().then(success, failure);
+		}
 	}, []);
 
-	return <div>{/* Wyświetl listę plików MIDI tutaj */}</div>;
+	return (
+		<div
+			onClick={async () => {
+				const filePath = await window.electronAPI.openFile();
+				// filePathElement.innerText = filePath
+				console.log(filePath);
+			}}
+		>
+			xd
+		</div>
+	);
 };
 
 export default App;
--- src/preload.ts
+++ src/preload.ts
@@ -1,15 +1,19 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge, ipcRenderer, ipcMain } from "electron";
 import * as fs from "fs";
 import * as path from "path";
 
 console.log("aha XD");
 
-contextBridge.exposeInMainWorld("electron", {
-	fs: fs,
-	path: path,
-	appPath: path.resolve(__dirname),
-	cwd: process.cwd,
-	appDir: __dirname,
+contextBridge.exposeInMainWorld("electronAPI", {
+	openFile: () => ipcRenderer.invoke("dialog:openFile"),
+	ipcRenderer,
+	receive: (channel, func) => {
+		let validChannels = ["app_ready"];
+		if (validChannels.includes(channel)) {
+			// Deliberately strip event as it includes `sender`
+			ipcRenderer.on(channel, (event, ...args) => func(...args));
+		}
+	},
 });
